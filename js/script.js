91dffba495c5bc25ceb244f96b4d969d62977c16
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-18 13:36:37 +0100

    refactor FIZZBUZZ logic with comments for clarity

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
-
+// Start For, inizializzo il ciclo
 for(let i = 1; i<=100;i++){
-    if(i % 3 === 0 && i % 5 === 0){
+
+    if(i % 3 === 0 && i % 5 === 0){  // Faccio un controllo se il numero e' multiplo sia di 3 che di 5
         console.log(`il numero ${i} e' multiplo sia per 3  che per 5 (FIZZBUZZ)`)
     }
-       else if(i % 5 === 0){
+       else if(i % 5 === 0){  //Faccio un controllo se il numero e' solo multiplo di 3
         console.log(`il numero ${i} e' multiplo di 5 (BUZZ)`)
     }
-    else if(i % 3 === 0){
+    else if(i % 3 === 0){ //Faccio un controllo se il numero e' solo multiplo di 5
         console.log(`il numero ${i} e' multiplo di 3 (FIZZ)`)
     }
     else{
