3ec9a0fcafec356c12e8dae16fd66e60e18e22a6
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-18 13:28:37 +0100

    [UPD] added check for multiples of 5 in the FIZZ logic

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@ for(let i = 1; i<=100;i++){
     if(i % 3 === 0){
         console.log(`il numero ${i} e' multiplo di 3 (FIZZ)`)
     }
+    else if(i % 5 === 0){
+        console.log(`il numero ${i} e' multiplo di 5 (FIZZ)`)
+    }
 }
