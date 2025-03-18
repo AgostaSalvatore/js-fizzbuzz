7ada25c8ba28da2a296a77c041b2a69121f2980c
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-18 13:28:56 +0100

    [UPD] added FIZZBUZZ logic for multiples of 3 and 5

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ for(let i = 1; i<=100;i++){
     if(i % 3 === 0){
         console.log(`il numero ${i} e' multiplo di 3 (FIZZ)`)
     }
-    else if(i % 5 === 0){
+       else if(i % 5 === 0){
         console.log(`il numero ${i} e' multiplo di 5 (FIZZ)`)
+    }
+    else if(i % 3 === 0 && i % 5 === 0){
+        console.log(`il numero ${i} e' multiplo sia per 3  che per 5 (FIZZBUZZ)`)
     }
 }
