73d9691c692b191a5360fae8f4a16f1440048cff
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-18 13:32:33 +0100

    [DONE]

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 
 for(let i = 1; i<=100;i++){
-    if(i % 3 === 0){
-        console.log(`il numero ${i} e' multiplo di 3 (FIZZ)`)
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(`il numero ${i} e' multiplo sia per 3  che per 5 (FIZZBUZZ)`)
     }
        else if(i % 5 === 0){
-        console.log(`il numero ${i} e' multiplo di 5 (FIZZ)`)
+        console.log(`il numero ${i} e' multiplo di 5 (BUZZ)`)
     }
-    else if(i % 3 === 0 && i % 5 === 0){
-        console.log(`il numero ${i} e' multiplo sia per 3  che per 5 (FIZZBUZZ)`)
+    else if(i % 3 === 0){
+        console.log(`il numero ${i} e' multiplo di 3 (FIZZ)`)
+    }
+    else{
+        console.log(i)
     }
 }
